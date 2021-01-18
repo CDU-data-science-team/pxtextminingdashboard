788280289414e23b49f3fe6c5a5746a646d44062
Andreas <andreassot10@gmail.com>
2021-01-18 06:15:51 +0000

    code: Add tooltips

--- R/mod_tidytext.R
+++ R/mod_tidytext.R
@@ -151,71 +151,27 @@ mod_tidytext_server <- function(id){
   
   output$hover_info <- renderUI({
     hover <- input$plot_hover
-    point <- nearPoints(plot_data(), hover, threshold = 25, maxpoints = 1, addDist = TRUE)
+    point <- nearPoints(plot_data(), hover, threshold = 250000000, maxpoints = 10, addDist = TRUE)
     if (nrow(point) == 0) return(NULL)
-    
-    # calculate point position INSIDE the image as percent of total dimensions
-    # from left (horizontal) and from top (vertical)
-    left_pct <- (hover$x - hover$domain$left) / 
-      (hover$domain$right - hover$domain$left)
-    top_pct <- (hover$domain$top - hover$y) / 
-      (hover$domain$top - hover$domain$bottom)
-    
-    # calculate distance from left and bottom side of the picture in pixels
-    left_px <- hover$range$left + 
-      left_pct * (hover$range$right - hover$range$left)
-    top_px <- hover$range$top + 
-      top_pct * (hover$range$bottom - hover$range$top)
     
     # create style property fot tooltip
     # background color is set so tooltip is a bit transparent
     # z-index is set so we are sure are tooltip will be on top
     style <- paste0("position:absolute; z-index:100; ",
-                    "left:", left_px + 2, "px; top:", top_px + 2, "px;")
+                    "left:", 0, "px; top:", 0, "px;")
 
     # actual tooltip created as wellPanel
     wellPanel(
       style = style,
-      p(HTML(paste0("<b> Car: </b>", 'a', "<br/>",
-                    #"<b> mpg: </b>", point$value, "<br/>",
-                    "<b> hp: </b>", 'B', "<br/>")))
+      p(HTML(paste0("<b> Tag: </b>", point$super[1], "<br/>",
+                    "<b>", point$name, ": </b>", point$value, "<br/>",
+                    "<b> Feedback text: </b>", point$improve[1], "<br/>")))
     )
   })
   
   output$facetPlot <- renderPlot({
     
-    break_text_into_several_lines <- function(x) {
-      
-      x %>%
-        quanteda::tokens() %>%
-        quanteda::tokens_chunk(size = 6) %>%
-        lapply(
-          function(x) {
-            paste0(paste(x, collapse = " "), "<br>")
-          }
-        ) %>%
-        unlist() %>%
-        paste(collapse = " ")
-    }
-    
-    tooltip_text <- function(name, value, feedback_text,
-                             line_breaking_function = 
-                               break_text_into_several_lines) {
-      
-      if (is.null(line_breaking_function)) {
-        fdbck_text <- feedback_text
-      } else {
-        fdbck_text <- line_breaking_function(feedback_text)
-      }
-      
-      paste0(
-        "<b>Sentiment:</b> ", name, "<br>",
-        "<b>Count</b>: ",  value, "<br>",
-        "<b>Feedback</b>: ", fdbck_text
-      )
-    }
-    
-    p <- plot_data() %>%
+    plot_data() %>%
       ggplot2::ggplot(ggplot2::aes(value, name)) +
       ggplot2::geom_col(fill = "blue", alpha = 0.6) + 
       ggplot2::facet_wrap(~ linenumber, ncol = 5) + 
@@ -228,8 +184,6 @@ mod_tidytext_server <- function(id){
         axis.ticks.x = ggplot2::element_blank()
       ) + 
       ggplot2::ylab('')
-    
-    p
   })
 })
 }
