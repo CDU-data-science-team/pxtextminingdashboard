d641a2fe6008a052c07064bc610c1fea3496dba7
Andreas <andreassot10@gmail.com>
2021-01-22 13:05:29 +0000

    code: Add hyperlink to TextBlob's polarity definition

--- R/mod_text_blob.R
+++ R/mod_text_blob.R
@@ -14,7 +14,7 @@ mod_text_blob_ui <- function(id){
     fluidRow(
       column(12,
              box(width = NULL, background = "red",
-                 textOutput(ns("textBlobBox"))
+                 htmlOutput(ns("textBlobBox"))
              )
       )
     ),
@@ -37,9 +37,10 @@ mod_text_blob_server <- function(id){
     ns <- session$ns
     
     output$textBlobBox <- renderText({
-      "Polarity is a way to calculate how positive or negative a 
+      HTML("<u><a href='https://textblob.readthedocs.io/en/dev/index.html'>
+          Polarity</a></u> is a way to calculate how positive or negative a 
        comment is. It ranges between -1 (very negative) to 
-       1 (very positive)."
+       1 (very positive).")
     })
     
     sticky_style <- list(position = "sticky", left = 0, 
