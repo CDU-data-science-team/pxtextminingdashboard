cb9a74365837e9e4ad7e59d4e09cd506a1d168f3
andreassot10 <andreassot10@gmail.com>
2021-06-02 14:08:49 +0100

    code: Move predictions table on test set to mod_performance_metrics.

--- R/mod_performance_metrics.R
+++ R/mod_performance_metrics.R
@@ -21,7 +21,21 @@ mod_performance_metrics_ui <- function(id) {
           box(
             width = NULL, 
             background = "red",
-            HTML("TO DO: Add a brief explanation how to read the matrix here.")
+            HTML(
+              "The <a target='_blank' rel='noopener noreferrer' href='https://en.wikipedia.org/wiki/Confusion_matrix'>Confusion Matrix</a> 
+              is a way to visually detect which classes the model has performed
+              best/worst in predicting. The actual classes are on the x-axis and
+              the predicted ones are on the y-axis. With a good model, we would
+              see most records on the diagonal. The integers correspond to the 
+              number of records belonging to the class on the x-axis that were 
+              predicted as the class on the y-axis. The shades of gray translate
+              these counts into proportions of the total number of records in 
+              the class on the x-axis. With a good model, the darker shades of
+              gray would be on the diagonal. The shades help spot where the most
+              severe misclassifications may have occurred. They thus help see if
+              the model is consistently confusing the text for a class on the
+              x-axis as being about a different class on the y-axis."
+            )
           )
         ),
         box(
@@ -33,6 +47,38 @@ mod_performance_metrics_ui <- function(id) {
             background = "red",
             htmlOutput(ns("modelPerformanceBox"))
           )
+        )
+      )
+    ),
+    
+    fluidRow(
+      column(
+        width = 12,
+        
+        box(
+          title = "Predicted text for each class",
+          width = NULL,
+          
+          box(
+            width = NULL, 
+            background = "red",
+            htmlOutput(ns("modelAccuracyBox"))
+          ),
+          
+          fluidRow(
+            column(
+              width = 6,
+              uiOutput(ns("classControl"))
+            ),
+            
+            column(
+              width = 6,
+              uiOutput(ns("organizationControl"))
+            )
+          ),
+          
+          reactable::reactableOutput(ns("predictedClasses")) %>%
+            shinycssloaders::withSpinner(hide.ui = FALSE)
         )
       )
     ),
@@ -64,8 +110,9 @@ mod_performance_metrics_ui <- function(id) {
 #' performance_metrics Server Functions
 #'
 #' @noRd 
-mod_performance_metrics_server <- function(id, x, target, target_pred, groups, 
-                                           preds, row_indices, tuning_results) {
+mod_performance_metrics_server <- function(id, x, target, target_pred, text_col,
+                                           groups, preds, row_indices, 
+                                           tuning_results) {
   moduleServer( id, function(input, output, session) {
     ns <- session$ns
     
@@ -87,6 +134,76 @@ mod_performance_metrics_server <- function(id, x, target, target_pred, groups,
           grouping_variables = NULL,
           type = "heatmap"
         )
+    })
+    
+    output$predictedClasses <- reactable::renderReactable({
+      
+      feedback_col_new_name <- paste0(
+        "Feedback that model predicted as ", "\"", input$class, "\""
+      )
+      
+      aux <- x %>%
+        dplyr::right_join(preds, by = "row_index") %>% 
+        dplyr::filter(
+          dplyr::across(
+            dplyr::all_of(target_pred),
+            ~ . %in% input$class
+          ),
+          dplyr::across(
+            dplyr::all_of(groups),
+            ~ . %in% input$organization
+          )
+        ) %>%
+        dplyr::select(dplyr::all_of(c(text_col, target, groups)))
+      
+      reactable_cols <- list(
+        reactable::colDef(name = feedback_col_new_name),
+        reactable::colDef(name = "Actual class", align = "right"),
+        reactable::colDef(name = "Organization", align = "right")
+      )
+      names(reactable_cols) <- c(text_col, target, groups[1])
+      
+      reactable::reactable(
+        aux,
+        # height = 1000, # Fix the height to make table scrollable and headers sticky.
+        columns = reactable_cols,
+        #rownames = TRUE,
+        searchable = TRUE,
+        sortable = FALSE,
+        defaultPageSize = 20,
+        pageSizeOptions = 20,
+        language = reactable::reactableLang(
+          searchPlaceholder = "Search for a word..."),
+      )
+    })
+    
+    output$modelAccuracyBox <- renderText({
+      
+      accuracy_score <- x %>% 
+        dplyr::select(dplyr::all_of(c(target, groups)), row_index) %>% 
+        dplyr::right_join(preds, by = "row_index") %>% 
+        experienceAnalysis::calc_accuracy_per_class(
+          target_col_name = target, 
+          target_pred_col_name = target_pred,
+          grouping_variables = groups,
+          column_names = NULL
+        ) %>% 
+        dplyr::filter(
+          dplyr::across(
+            dplyr::all_of(target),
+            ~ . %in% input$class
+          ),
+          dplyr::across(
+            dplyr::all_of(groups),
+            ~ . %in% input$organization
+          )
+        ) %>%
+        dplyr::select(accuracy) %>%
+        dplyr::mutate(accuracy = round(accuracy * 100)) %>%
+        dplyr::pull()
+      
+      HTML(paste0(
+        "Learner accuracy for this class is ", accuracy_score, "%."))
     })
     
     output$rawMetrics <- reactable::renderReactable({
@@ -128,5 +245,28 @@ mod_performance_metrics_server <- function(id, x, target, target_pred, groups,
       content = function(file) {
         write.csv(tuning_results, file)
       })
+    
+    output$classControl <- renderUI({
+      
+      aux <- x %>%
+        dplyr::right_join(row_indices, by = 'row_index')
+      
+      selectInput(
+        session$ns("class"), 
+        "Choose a class:",
+        choices = sort(unique(unlist(aux[[target]]))),
+        selected = sort(unique(unlist(aux[[target]])))[1]
+      )
+    })
+    
+    output$organizationControl <- renderUI({
+      
+      selectInput(
+        session$ns("organization"), 
+        "Choose an organization:",
+        choices = sort(unique(x[[groups[1]]])), # The first group is always the "main" one (see {experienceAnalysis}), i.e. the Trust/Organization in the Patient Experience case.
+        selected = sort(unique(x[[groups[1]]]))[1]
+      )
+    })
   })
 }
